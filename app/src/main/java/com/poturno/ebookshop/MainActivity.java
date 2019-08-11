af930d32307d3d58cf7f67f3feb6cc9254f2ecf3
Vitor Poncell <vitorponcell@hotmail.com>
2019-08-11 17:07:16 -0300

    class 49

--- app/src/main/java/com/poturno/ebookshop/MainActivity.java
+++ app/src/main/java/com/poturno/ebookshop/MainActivity.java
@@ -1,13 +1,23 @@
 package com.poturno.ebookshop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.poturno.ebookshop.databinding.ActivityMainBinding;
 import com.poturno.ebookshop.model.Book;
 import com.poturno.ebookshop.model.Category;
 import com.poturno.ebookshop.persistence.EBookShopRepository;
@@ -19,32 +29,68 @@ public class MainActivity extends AppCompatActivity {
 
     private String TAG = "MainActivity";
     private MainActivityViewModel mainActivityViewModel;
+    private ArrayList<Category> categoriesList;
+    private ActivityMainBinding activityMainBinding;
+    private MainActivityClickHandlers handlers;
+    private Category selectedCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EBookShopRepository eBookShopRepository = new EBookShopRepository(getApplication());
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        handlers = new MainActivityClickHandlers();
+        activityMainBinding.setClickHandlers(handlers);
 
         mainActivityViewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
 
-        Log.d(TAG,"before");
+        Log.d(TAG, "before");
         mainActivityViewModel.getAllCategories().observe(this, new Observer<List<Category>>() {
             @Override
             public void onChanged(List<Category> categories) {
-                Log.d(TAG,"onChanged");
+                categoriesList = (ArrayList<Category>) categories;
+                for (Category category : categories) {
+                    Log.d(TAG, category.getCategoryName());
+                }
+
+                showOnSpinner();
             }
         });
-        Log.d(TAG,"after");
-
         mainActivityViewModel.getBooksOfASelectedCategory(3).observe(this, new Observer<List<Book>>() {
             @Override
             public void onChanged(List<Book> books) {
-                 for (Book book : books){
-                     Log.d(TAG,book.getBookName());
-                 }
+                for (Book book : books) {
+                    Log.d(TAG, book.getBookName());
+                }
             }
         });
+
+    }
+
+    private void showOnSpinner() {
+
+        ArrayAdapter<Category> categoryArrayAdapter = new ArrayAdapter<Category>(this, R.layout.spinner_item, categoriesList);
+        categoryArrayAdapter.setDropDownViewResource(R.layout.spinner_item);
+        activityMainBinding.setSpinnerAdapter(categoryArrayAdapter);
+
+    }
+
+
+    public class MainActivityClickHandlers {
+
+        public void onFABClicked(View view) {
+            Toast.makeText(getApplicationContext(), "FAB clicked", Toast.LENGTH_LONG).show();
+        }
+
+        public void onSelectItem(AdapterView<?> parent, View view, int pos, long id) {
+
+            selectedCategory = (Category) parent.getItemAtPosition(pos);
+
+            String message = " id is " + selectedCategory.getCategoryId() + "\n name is " + selectedCategory.getCategoryName() + "\n email is " + selectedCategory.getCategoryDescription();
+
+            Toast.makeText(parent.getContext(), message, Toast.LENGTH_LONG).show();
+
+        }
     }
 }
